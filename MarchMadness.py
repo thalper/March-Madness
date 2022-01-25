d4b8d16c657753bc0f1971db8f64f7ec153ace4a
Tycho <thalper@purdueedu>
2022-01-25 17:12:36 -0500

    UPDATE parseYear()

--- MarchMadness.py
+++ MarchMadness.py
@@ -1,20 +1,18 @@
 import numpy as np
 import csv
 
+
 def parseYear(year, teamSet): # input year, output numpy array storing statistics of the 68 march madness teams from that year
     dataStr = "Previous/cbb"+str(year%2000)+".csv"
-    file = open(dataStr, 'r')
-    line = file.readline() # reads header so it is not interpreted as data
-    
-    while line:
-        line = file.readline()
-        if line[:10] in teamSet:
-            print(line)
-            # add new row to numpy array that includes data for the team
-    file.close()
+    with open(dataStr, newline='') as csvfile:
+        dataByTeam = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in dataByTeam:
+            if row[0] in teamSet:
+                print(row)
 
 if __name__ == "__main__":
-    playoffTeams = [] #list of sets, each set contains the name of the schools that competed in March Madness
+    test = {"Duke", "Purdue", "Michigan", "UNLV"}
+    playoffTeams = [test]*9 #list of sets, each set contains the name of the schools that competed in March Madness
     for year in range(2013,2022):
         print(year)
         teamSet = playoffTeams[year%2013]
