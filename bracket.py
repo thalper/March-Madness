0a6aebc5a4dd31a524477d144c15637dddcfc8aa
rbenes20 <rbenes@purdue.edu>
2022-02-23 15:51:10 -0500

    ADDED .JPG OUTPUT

--- bracket.py
+++ bracket.py
@@ -1,4 +1,5 @@
 import tkinter
+from PIL import Image, ImageDraw, ImageFont
 
 # https://github.com/JonathanZwiebel/bracket-generator
 root = tkinter.Tk()
@@ -13,9 +14,10 @@ def addXY(boxKey):
     # CHANGE FOR TEAM NAMES NOW
     # BOXKEY SHOULD BE TEAM NAMES
     xValue = x_center - _game_box_width / 16
-    yValue = y_center - _game_box_height / 16
+    yValue = y_center - _game_box_height / 16 - 2
     bracketLoc[boxKey] = [xValue, yValue]
     canvas.create_text(bracketLoc[boxKey][0], bracketLoc[boxKey][1], text=boxKey, fill="black", font=('Helvetica 5 bold'))
+    draw.text((bracketLoc[boxKey][0], bracketLoc[boxKey][1]), text=str(boxKey), fill="black", font=ImageFont.load_default())
 
 if __name__ == "__main__":
     _size = 5
@@ -25,6 +27,8 @@ if __name__ == "__main__":
     _game_box_height = _game_box_width / GAME_BOX_WIDTH_HEIGHT_RATIO
     canvas = tkinter.Canvas(root, width=WIDTH, height=HEIGHT)
     canvas.pack()
+    image1 = Image.new("RGB", (WIDTH, HEIGHT), 'white')
+    draw = ImageDraw.Draw(image1)
     for i in range(_columns):
         if i - _size < 0:
             side = "LEFT"
@@ -38,18 +42,23 @@ if __name__ == "__main__":
         for j in range(games):
             y_center = y_size * (j + 0.5)
             canvas.create_rectangle(x_center - _game_box_width / 2, y_center - _game_box_height / 2, x_center + _game_box_width / 2, y_center + _game_box_height / 2)
+            draw.rectangle([x_center - _game_box_width / 2, y_center - _game_box_height / 2, x_center + _game_box_width / 2, y_center + _game_box_height / 2], outline = 'black')
             if i <= 10: #creates dictionary of x, y coords on the bracket L to R, top to bottom
                 addXY(boxKey)
                 boxKey += 1
             if i != _columns - 1:
                 canvas.create_line(x_center + _game_box_width / 2, y_center, x_center + _game_box_width / 2 + HORIZONTAL_PADDING / 2, y_center)
+                draw.line([x_center + _game_box_width / 2, y_center, x_center + _game_box_width / 2 + HORIZONTAL_PADDING / 2, y_center], 'black')
             if i != 0:
                 canvas.create_line(x_center - _game_box_width / 2, y_center, x_center - _game_box_width / 2 - HORIZONTAL_PADDING / 2, y_center)
-
+                draw.line([x_center - _game_box_width / 2, y_center, x_center - _game_box_width / 2 - HORIZONTAL_PADDING / 2, y_center], 'black')
             if j % 2 == 1:
                 if side == "LEFT":
                     canvas.create_line(x_center + _game_box_width / 2 + HORIZONTAL_PADDING / 2, y_center, x_center + _game_box_width / 2 + HORIZONTAL_PADDING / 2, y_center - y_size)
+                    draw.line([x_center + _game_box_width / 2 + HORIZONTAL_PADDING / 2, y_center, x_center + _game_box_width / 2 + HORIZONTAL_PADDING / 2, y_center - y_size], 'black')
                 if side == "RIGHT":
                     canvas.create_line(x_center - _game_box_width / 2 - HORIZONTAL_PADDING / 2, y_center, x_center - _game_box_width / 2 - HORIZONTAL_PADDING / 2, y_center - y_size)
-
+                    draw.line([x_center - _game_box_width / 2 - HORIZONTAL_PADDING / 2, y_center, x_center - _game_box_width / 2 - HORIZONTAL_PADDING / 2, y_center - y_size], 'black')
+    filename = "myBracket.jpg"
+    image1.save(filename)
     tkinter.mainloop()
