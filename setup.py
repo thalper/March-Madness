62d4b5488d5e5c689e26e3ac8091d9417151434a
Tycho <thalper@purdueedu>
2022-03-22 16:32:22 -0400

    add Manifest file, update setup.py, create new build distribution

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='MarchMadness',  
      version='0.1',
-     scripts=['MarchMadness.py', 'Simulate.py'] ,
+     scripts=['MarchMadness.py', 'Simulate.py', 'AIWeighting.py', 'bracket.py'] ,
+     include_package_data=True,
+     package_data={'Previous': ['*']},
      author="Tycho Halpern, Rylee Benes, Luca Rivera",
      author_email="thalper@purdue.edu,rbenes@purdue.edu, river172@purdue.edu",
      description="March Madness bracket simulation 2022",
