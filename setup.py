ea98348d8ccce075662c3cd24cdf5c097a799487
Tycho <thalper@purdueedu>
2022-03-24 13:59:45 -0400

    new build

--- setup.py
+++ setup.py
@@ -3,11 +3,29 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+REQUIRES = [ 
+    "pytest",
+    "mypy",
+    "numpy",
+    "coverage",
+    "pytest-cov",
+    "pillow",
+    "collections",
+    "math",
+    "re",
+    "string",
+    "csv",
+    "sklearn",
+    "cv2",
+    "random",
+    "time",
+    "inspect"]
 
 setuptools.setup(
      name='MarchMadness',  
      version='1.0',
      scripts=['MarchMadness.py', 'Simulate.py', 'AIWeighting.py', 'bracket.py'] ,
+     install_requires=REQUIRES,
      include_package_data=True,
      package_data={'Previous': ['*']},
      author="Tycho Halpern, Rylee Benes, Luca Rivera",
