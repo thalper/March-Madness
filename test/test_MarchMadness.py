9c2343bc2c8f9c28ddec2b5e23144afbee2919be
Tycho Halpern <thalper@purdue.edu>
2022-04-12 17:07:44 -0400

    change test cases to increase coverage and speed up tests

--- test/test_MarchMadness.py
+++ test/test_MarchMadness.py
@@ -23,9 +23,9 @@ def test_tournament2():
   regressions = MarchMadness.MarchMadnessRun.setRegressions() # calculate regregression models using data from previous tournaments
 
   # change these values to generate real brackets
-  gameTest = [5] # number of games to simulate per matchup, higher number here leads to less variation in generated brackets
+  gameTest = [1] # number of games to simulate per matchup, higher number here leads to less variation in generated brackets
   yearTest = [2021, 2022] # what year(s) do you want to generate brackets for?
-  numBrackets = 10 # how many unique brackets do you want to generate?
+  numBrackets = 25 # how many unique brackets do you want to generate?
   for year in yearTest:
       for numGames in gameTest:
           assert MarchMadness.MarchMadnessRun.tournament(year, regressions, output, numGames, numBrackets) == True
