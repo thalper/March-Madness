1876a5d81276e2d8b7cf3575fc6889579a7c7cd2
Tycho Halpern <thalper@purdue.edu>
2022-04-12 16:50:35 -0400

    remove unnecessary lines from AIWeighting.py

--- MarchMadness/AIWeighting.py
+++ MarchMadness/AIWeighting.py
@@ -13,32 +13,31 @@ import MarchMadness.Previous
 
 prevData = {}
 
-
-def parseData():
-    for year in range(2013,2023):
-        MarchMadness.dataSet.append({}) # each key is a team, the value is the data for that team
-        if year == 2020:
-            continue
-        parseYear(year)
-    parsePrevTourneyforAI(MarchMadness.dataSet)
+# def parseData():
+#     for year in range(2013,2023):
+#         MarchMadness.dataSet.append({}) # each key is a team, the value is the data for that team
+#         if year == 2020:
+#             continue
+#         parseYear(year)
+#     parsePrevTourneyforAI(MarchMadness.dataSet)
     
 
-def parseYear(year): # input year, output dict of numpy array storing statistics of the 68 march madness teams from that year
-    dataStr = "Previous/tr"+str(year%2000)+".csv"
-    teamsFileStr = "Previous/teams"+str(year%2000)+".txt"
-    teamFile = open(teamsFileStr, "r")
-    tourneyTeams = set(teamFile.read().split('\n'))
-    teamFile.close()
-    with open(dataStr, newline='') as csvfile:
-        dataByTeam = csv.reader(csvfile, delimiter=',', quotechar='|')
-        ind = year%2013   
-        if year > 2020:
-            ind -= 1
-        for row in dataByTeam:
-            if row[0] in tourneyTeams:
-                row[5] = str(float(row[5]) - float(row[1]))
-                MarchMadness.dataSet[ind][row[0]] = np.array(row[1:22], dtype=float)
-                tourneyTeams.remove(row[0])
+# def parseYear(year): # input year, output dict of numpy array storing statistics of the 68 march madness teams from that year
+#     dataStr = "Previous/tr"+str(year%2000)+".csv"
+#     teamsFileStr = "Previous/teams"+str(year%2000)+".txt"
+#     teamFile = open(teamsFileStr, "r")
+#     tourneyTeams = set(teamFile.read().split('\n'))
+#     teamFile.close()
+#     with open(dataStr, newline='') as csvfile:
+#         dataByTeam = csv.reader(csvfile, delimiter=',', quotechar='|')
+#         ind = year%2013   
+#         if year > 2020:
+#             ind -= 1
+#         for row in dataByTeam:
+#             if row[0] in tourneyTeams:
+#                 row[5] = str(float(row[5]) - float(row[1]))
+#                 MarchMadness.dataSet[ind][row[0]] = np.array(row[1:22], dtype=float)
+#                 tourneyTeams.remove(row[0])
 
 def parsePrevTourneyforAI(dataSet):
     # dataStr = "Previous/NCAATourneyFullBoxscoresAndStats_15-19.csv" # previous tournament data box scores
@@ -80,47 +79,45 @@ def parsePrevTourneyforAI(dataSet):
             prevData[gameKey] = GameStats
 
 #file for creating AI (naive Bayes, etc) for creating weights for later simulation
-def createWeights():
-    numWeights = 24 # 
-    weights = [0.5]*numWeights  # initialization before we do Naive Bayes analysis
-    temp = w3pta(12, weights[0], 8, weights[1])
-    return weights 
+# def createWeights():
+#     numWeights = 24 # 
+#     weights = [0.5]*numWeights  # initialization before we do Naive Bayes analysis
+#     temp = w3pta(12, weights[0], 8, weights[1])
+#     return weights 
 
-def w3pta(Ataken, wAT, Ballowed, WBA):
-    return Ataken*wAT + Ballowed*WBA
+# def w3pta(Ataken, wAT, Ballowed, WBA):
+#     return Ataken*wAT + Ballowed*WBA
 
-def regression(xData, yData):
+# def regression(xData, yData):
 
-    X_train, X_test, Y_train, Y_test = train_test_split(xData, yData, test_size = 0.2)
+#     X_train, X_test, Y_train, Y_test = train_test_split(xData, yData, test_size = 0.2)
     
-    regr = LinearRegression()
-    regr.fit(X_train,Y_train)
-    y_pred = regr.predict(X_test)
+#     regr = LinearRegression()
+#     regr.fit(X_train,Y_train)
+#     y_pred = regr.predict(X_test)
 
-    print(X_test, y_pred)
+#     print(X_test, y_pred)
 
 def manualRegression(X_train, Y_train):
     regr = LinearRegression()
     regr.fit(X_train,Y_train)
-    
-
     return regr
 
-if __name__ == "__main__":
-    for year in range(2013,2023):
-        MarchMadness.dataSet.append({})
-    parseData()
-    xData = []
-    yData = []
-    for key in prevData:
-        Away = key[0]
-        Home = key[1]
-        year = key[2]
-        currX = [MarchMadness.dataSet[year%2013][Away][0], MarchMadness.dataSet[year%2013][Home][2]]
-        currY = prevData[key][0][0]
-        xData.append(currX)
-        yData.append(currY)
-    # xData = [[12.4, 14.2],[13.7, 12.1],[14.3,16.2],[11.2,15.6],[12.6,15.2],[12.0, 15.2],[13.7, 12.1],[14.7,16.2],[11.2,15.6],[12.6,15.2]]
-    # yData = [13, 14, 15, 12, 15, 13, 14, 15, 12, 15]
-    regr = manualRegression(xData, yData)
-    print(regr.predict([[18.5, 22.7]]))
+# if __name__ == "__main__":
+#     for year in range(2013,2023):
+#         MarchMadness.dataSet.append({})
+#     parseData()
+#     xData = []
+#     yData = []
+#     for key in prevData:
+#         Away = key[0]
+#         Home = key[1]
+#         year = key[2]
+#         currX = [MarchMadness.dataSet[year%2013][Away][0], MarchMadness.dataSet[year%2013][Home][2]]
+#         currY = prevData[key][0][0]
+#         xData.append(currX)
+#         yData.append(currY)
+#     # xData = [[12.4, 14.2],[13.7, 12.1],[14.3,16.2],[11.2,15.6],[12.6,15.2],[12.0, 15.2],[13.7, 12.1],[14.7,16.2],[11.2,15.6],[12.6,15.2]]
+#     # yData = [13, 14, 15, 12, 15, 13, 14, 15, 12, 15]
+#     regr = manualRegression(xData, yData)
+#     print(regr.predict([[18.5, 22.7]]))
