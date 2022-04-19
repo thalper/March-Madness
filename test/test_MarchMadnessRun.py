898c5c15f52e9ac2b9626cb2cd3f5e64f9cb6bf0
Tycho Halpern <thalper@purdue.edu>
2022-04-19 16:44:46 -0400

    add new test cases where play in teams are picked to win it all

--- test/test_MarchMadnessRun.py
+++ test/test_MarchMadnessRun.py
@@ -65,6 +65,41 @@ def test_tournament4(monkeypatch):
           assert MarchMadness.MarchMadnessRun.tournament(year, regressions, output, numGames, numBrackets, champion) == True
         
 
+def test_tournament5(monkeypatch):
+  monkeypatch.setattr('builtins.input', lambda _: "y")
+  MarchMadness.MarchMadnessRun.parseData() # creates dataset
+  output = [0]*127 # used to store team names for printing final bracket
+
+  regressions = MarchMadness.MarchMadnessRun.setRegressions() # calculate regregression models using data from previous tournaments
+
+  # change these values to generate real brackets
+  gameTest = [1] # number of games to simulate per matchup, higher number here leads to less variation in generated brackets
+  yearTest = [2022] # what year(s) do you want to generate brackets for?
+  numBrackets = 5 # how many unique brackets do you want to generate?
+  champion = "Indiana"
+  for year in yearTest:
+      for numGames in gameTest:
+          assert MarchMadness.MarchMadnessRun.tournament(year, regressions, output, numGames, numBrackets, champion) == True
+        
+
+def test_tournament6(monkeypatch):
+  monkeypatch.setattr('builtins.input', lambda _: "y")
+  MarchMadness.MarchMadnessRun.parseData() # creates dataset
+  output = [0]*127 # used to store team names for printing final bracket
+
+  regressions = MarchMadness.MarchMadnessRun.setRegressions() # calculate regregression models using data from previous tournaments
+
+  # change these values to generate real brackets
+  gameTest = [1] # number of games to simulate per matchup, higher number here leads to less variation in generated brackets
+  yearTest = [2022] # what year(s) do you want to generate brackets for?
+  numBrackets = 5 # how many unique brackets do you want to generate?
+  champion = "*Indiana"
+  for year in yearTest:
+      for numGames in gameTest:
+          assert MarchMadness.MarchMadnessRun.tournament(year, regressions, output, numGames, numBrackets, champion) == True
+        
+
+
 
 
   
